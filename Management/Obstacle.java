6952d5c5eb9a4108fbbff55ebc24e837cf25149b
GuskoVVadia <guskov.vadim@gmail.com>
2019-09-17 00:07:06 +0300

    home work

---
+++ Management/Obstacle.java
@@ -0,0 +1,5 @@
+package Management;
+
+public abstract class Obstacle {
+    public abstract void doIt(Competitor competitor);
+}
---
+++ Participants/Cat.java
@@ -0,0 +1,9 @@
+package Participants;
+
+import Management.Animal;
+
+public class Cat extends Animal {
+    public Cat(String name) {
+        super("Кот", name, 200, 20, 0);
+    }
+}
